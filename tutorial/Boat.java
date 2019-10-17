af8a9bd1d36b520d9b3168d5d3c8ca0fd1b5e8e9
oscar yasael <oscar_yasael@hotmail.com>
2019-10-17 10:59:12 -0500

    bote
    se agrego un vote y un mobimiento

---
+++ tutorial/Boat.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Boat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boat extends Actor
+{
+    /**
+     * Act - do whatever the Boat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        move();
+    }    
+    
+    public void move()
+    {
+        int x = super.getX();
+        int y = super.getY();
+        this.setLocation(x +1 , 1); 
+    }
+}
